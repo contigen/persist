432ba612c9fdf9e3570390197d6548664c12ba07
contigen <69436315+contigen@users.noreply.github.com>
2024-07-19 19:59:59 +0100

    Update state vars

--- src/features/qr-code-scanner.tsx
+++ src/features/qr-code-scanner.tsx
@@ -1,19 +1,30 @@
 import { IDetectedBarcode, Scanner } from '@yudiel/react-qr-scanner'
 import { useState } from 'react'
+import { Spinner } from './ui/spinner'
+import { BASE_URL } from '../utils'
 
 export function QRCodeScanner() {
   const [showScanner, setShowScanner] = useState(false)
-  const [result, setResult] = useState<IDetectedBarcode[]>([])
+  const [result, setResult] = useState(``)
+
+  function handleResult(result: IDetectedBarcode[]) {
+    const resolvedResult = result.map(r => r.rawValue).join(``)
+    setResult(resolvedResult)
+    if (resolvedResult.startsWith(BASE_URL)) {
+      window.location.href = resolvedResult
+    }
+  }
   return (
     <div>
       <h1>QR Code Scanner</h1>
       <button onClick={() => setShowScanner(prev => !prev)}>
         Show Scanner
       </button>
-      {showScanner && <Scanner onScan={result => setResult(result)} />}
+      {Spinner}
+      {showScanner && <Scanner onScan={handleResult} />}
       {result && (
-        <h3 style={{ letterSpacing: `-0.04em` }}>
-          {result.map(r => r.rawValue)}
+        <h3 style={{ letterSpacing: `-0.04em`, wordWrap: `break-word` }}>
+          {result}
         </h3>
       )}
     </div>
