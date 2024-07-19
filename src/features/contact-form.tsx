480c855175b92e47ccbb2f49ed2ffa0b4aa3f299
contigen <69436315+contigen@users.noreply.github.com>
2024-07-19 03:01:33 +0100

    Add contact form

---
+++ src/features/contact-form.tsx
@@ -0,0 +1,39 @@
+import { ChangeEvent, useEffect, useState } from 'react'
+
+export function ContactForm() {
+  const [formState, setFormState] = useState({
+    name: ``,
+    number: ``,
+    info: ``,
+  })
+
+  function handleChange({
+    target: { name, value },
+  }: ChangeEvent<HTMLInputElement>) {
+    setFormState(prev => ({ ...prev, [name]: value }))
+  }
+  return (
+    <form>
+      <input
+        type='text'
+        placeholder='Name'
+        name='name'
+        onChange={handleChange}
+      />
+      <input
+        type='text'
+        placeholder='Number'
+        pattern='[0-9]'
+        inputMode='numeric'
+        name='number'
+        onChange={handleChange}
+      />
+      <input
+        type='text'
+        placeholder='Additional Info'
+        name='info'
+        onChange={handleChange}
+      />
+    </form>
+  )
+}
