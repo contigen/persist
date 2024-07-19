6c9baadb5d24ad47806690b0a59d52fd14c42380
contigen <69436315+contigen@users.noreply.github.com>
2024-07-19 05:04:18 +0100

    Add helper functions

---
+++ src/utils.ts
@@ -0,0 +1,28 @@
+type VCardContact = {
+  name: string[]
+  tel: string[]
+}
+
+export function generateVCard(contacts: VCardContact[]): string {
+  return contacts
+    .map(contact => {
+      return `BEGIN:VCARD
+  VERSION:3.0
+  FN:${contact.name[0]}
+  TEL:${contact.tel[0]}
+  END:VCARD`
+    })
+    .join(`\n`)
+}
+
+export function downloadVCard(vCardText: string) {
+  const blob = new Blob([vCardText], { type: `text/vcard` })
+  const url = URL.createObjectURL(blob)
+  const link = document.createElement(`a`)
+  link.href = url
+  link.download = `contacts.vcf`
+  document.body.appendChild(link)
+  link.click()
+  document.body.removeChild(link)
+  URL.revokeObjectURL(url)
+}
