24c9a9e63211ff7040436e68fbd5fbde4186a395
contigen <69436315+contigen@users.noreply.github.com>
2024-07-19 19:58:37 +0100

    Add spinner

---
+++ src/features/ui/spinner.tsx
@@ -0,0 +1,10 @@
+import { RotatingLines } from 'react-loader-spinner'
+
+export const Spinner = (
+  <RotatingLines
+    strokeColor='#000'
+    strokeWidth='4'
+    animationDuration='0.75'
+    width='32'
+  />
+)
