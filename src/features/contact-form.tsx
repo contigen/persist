24909aa779242a1a078ec5a7431b6eb8ffe2dedd
contigen <69436315+contigen@users.noreply.github.com>
2024-07-19 18:00:25 +0100

    Update form

--- src/features/contact-form.tsx
+++ src/features/contact-form.tsx
@@ -1,39 +1,60 @@
-import { ChangeEvent, useState } from 'react'
+import { ChangeEvent, FormEvent, useState } from 'react'
+import QRCode from 'react-qr-code'
+import { generateVCard } from '../utils'
 
 export function ContactForm() {
-  const [, setFormState] = useState({
+  const [formState, setFormState] = useState({
     name: ``,
     number: ``,
     info: ``,
   })
+  const [vCardText, setVCardText] = useState(``)
 
   function handleChange({
     target: { name, value },
   }: ChangeEvent<HTMLInputElement>) {
     setFormState(prev => ({ ...prev, [name]: value }))
   }
+
+  function handleSubmit(evt: FormEvent<HTMLFormElement>) {
+    evt.preventDefault()
+    const $form = evt.currentTarget
+    const formData = new FormData($form)
+    console.log(`Form submitted!`, formData.get(`name`), formData.get(`number`))
+    const { name, number } = formState
+    setVCardText(generateVCard([{ name, tel: number }]))
+  }
   return (
-    <form>
-      <input
-        type='text'
-        placeholder='Name'
-        name='name'
-        onChange={handleChange}
-      />
-      <input
-        type='text'
-        placeholder='Number'
-        pattern='[0-9]'
-        inputMode='numeric'
-        name='number'
-        onChange={handleChange}
-      />
-      <input
-        type='text'
-        placeholder='Additional Info'
-        name='info'
-        onChange={handleChange}
-      />
-    </form>
+    <>
+      <h1>Add contact</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          placeholder='Name'
+          name='name'
+          onChange={handleChange}
+        />
+        <input
+          type='text'
+          placeholder='Number'
+          inputMode='numeric'
+          name='number'
+          onChange={handleChange}
+        />
+        <input
+          type='text'
+          placeholder='Additional Info'
+          name='info'
+          onChange={handleChange}
+        />
+        <button type='submit'>Add Contact</button>
+      </form>
+      <br />
+      {vCardText && (
+        <QRCode
+          value={`https://persist-tau.vercel.app?vCardText=${vCardText}`}
+        />
+      )}
+    </>
   )
 }
