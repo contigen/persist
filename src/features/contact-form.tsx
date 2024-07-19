4c09f9500ecd020813ed06c114eef7e1a3a9e9b5
contigen <69436315+contigen@users.noreply.github.com>
2024-07-19 19:36:58 +0100

    Remove state-based inputs' logic

--- src/features/contact-form.tsx
+++ src/features/contact-form.tsx
@@ -1,52 +1,30 @@
-import { ChangeEvent, FormEvent, useState } from 'react'
+import { FormEvent, useState } from 'react'
 import QRCode from 'react-qr-code'
 import { generateVCard } from '../utils'
 
 export function ContactForm() {
-  const [formState, setFormState] = useState({
-    name: ``,
-    number: ``,
-    info: ``,
-  })
   const [vCardText, setVCardText] = useState(``)
-
-  function handleChange({
-    target: { name, value },
-  }: ChangeEvent<HTMLInputElement>) {
-    setFormState(prev => ({ ...prev, [name]: value }))
-  }
 
   function handleSubmit(evt: FormEvent<HTMLFormElement>) {
     evt.preventDefault()
     const $form = evt.currentTarget
     const formData = new FormData($form)
-    console.log(`Form submitted!`, formData.get(`name`), formData.get(`number`))
-    const { name, number } = formState
+    let { name, number } = Object.fromEntries(formData)
+    ;(name = name.toString()), (number = number.toString())
     setVCardText(generateVCard([{ name, tel: number }]))
   }
   return (
     <>
       <h1>Add contact</h1>
       <form onSubmit={handleSubmit}>
-        <input
-          type='text'
-          placeholder='Name'
-          name='name'
-          onChange={handleChange}
-        />
+        <input type='text' placeholder='Name' name='name' />
         <input
           type='text'
           placeholder='Number'
           inputMode='numeric'
           name='number'
-          onChange={handleChange}
         />
-        <input
-          type='text'
-          placeholder='Additional Info'
-          name='info'
-          onChange={handleChange}
-        />
+        <input type='text' placeholder='Additional Info' name='info' />
         <button type='submit'>Add Contact</button>
       </form>
       <br />
