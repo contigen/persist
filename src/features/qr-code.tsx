4856442d05da1b28874804a14dd4d4911b2b888d
contigen <69436315+contigen@users.noreply.github.com>
2024-07-20 20:12:00 +0100

    Add download QR Code feat

--- src/features/qr-code.tsx
+++ src/features/qr-code.tsx
@@ -1,13 +1,30 @@
 import QRCode from 'react-qr-code'
+import { toJpeg } from 'html-to-image'
 import { BASE_URL } from '../utils'
+import { useRef } from 'react'
 
 export function QRCodeView({ value }: { value: string }) {
   value = encodeURIComponent(value)
+  const QRCodeContainerRef = useRef<HTMLDivElement | null>(null)
+
+  function downloadQRCode() {
+    QRCodeContainerRef.current &&
+      toJpeg(QRCodeContainerRef.current).then(dataUrl => {
+        const link = document.createElement(`a`)
+        link.download = `contact-qr_code.jpeg`
+        link.href = dataUrl
+        link.click()
+      })
+  }
   return (
     <div>
-      <h1>QR Code.</h1>
-      <div className=''>
-        <QRCode value={`${BASE_URL}?vCardText=${value}`} />
+      <h2 className='font-[640]'>Quick Response Code.</h2>
+      <br />
+      <div className='flex justify-center flex-col items-center'>
+        <div className='p-3 bg-white' ref={QRCodeContainerRef}>
+          <QRCode value={`${BASE_URL}?vCardText=${value}`} />
+        </div>
+        <button onClick={downloadQRCode}>Download QR Code</button>
       </div>
     </div>
   )
