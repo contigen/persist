b37391c1b6cbd738eabf3c244292b5f295986827
contigen <69436315+contigen@users.noreply.github.com>
2024-07-20 10:52:51 +0100

    Add base URL

--- src/features/qr-code.tsx
+++ src/features/qr-code.tsx
@@ -1,11 +1,12 @@
 import QRCode from 'react-qr-code'
+import { BASE_URL } from '../utils'
 
 export function QRCodeView({ value }: { value: string }) {
   return (
     <div>
       <h1>QR Code.</h1>
       <div className=''>
-        <QRCode value={value} />
+        <QRCode value={`${BASE_URL}?vCardText=${value}`} />
       </div>
     </div>
   )
