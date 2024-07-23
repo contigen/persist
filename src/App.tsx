3d1f0d486f2f09f64eaa5dd7709575e0c18d5c2e
contigen <69436315+contigen@users.noreply.github.com>
2024-07-23 18:00:09 +0100

    Add logo for mobile

--- src/App.tsx
+++ src/App.tsx
@@ -31,21 +31,24 @@ function App() {
 
   return (
     <div vaul-drawer-wrapper='' className='bg-white min-h-[100vh]'>
-      <header className='bg-gradient-to-tl from-slate-900 via-neutral-400 to-blue-800 p-20 space-y-8'>
+      <header className='bg-gradient-to-tl from-slate-900 via-neutral-400 to-blue-800 p-20 space-y-8 hidden md:block'>
         <h1 className='leading-[4rem] text-balance'>
           Persist: transfer contacts across devices on the Web.
         </h1>
-        <p className='tracking-tight text-2xl'>
+        <p className='tracking-tight text-xl md:text-2xl'>
           A contact sharing app using the Contact Picker API & QR Codes to embed
           data for seamless transfer across devices.
         </p>
       </header>
       <div className='p-4'>
-        <span className='inline-block p-2'>
-          <InlineBubble direction='left' className='py-1.5 px-4'>
-            <Book size='24' color='#fff' variant='TwoTone' />
-          </InlineBubble>
-        </span>
+        <h1 className='block sm:hidden'>
+          <span className='inline-block p-2'>
+            <InlineBubble direction='left' className='py-1.5 px-4'>
+              <Book size='24' color='#fff' variant='TwoTone' />
+            </InlineBubble>
+            Persist.
+          </span>
+        </h1>
         <br />
         <PersonalContactWithQRCode />
         <SharedContactWithQRCode />
