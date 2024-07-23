c9f2e7fa30a1eb8ed39af62635d0e3198ebd138b
contigen <69436315+contigen@users.noreply.github.com>
2024-07-23 18:02:20 +0100

    Fix tooltip display

--- src/features/ui/inline-bubble.tsx
+++ src/features/ui/inline-bubble.tsx
@@ -11,9 +11,9 @@ export function InlineBubble({ direction, children, className }: BubbleProps) {
   return (
     <span
       className={cn(
-        `relative rounded-3xl text-white px-3 py-px before:absolute after:absolute before:content-[''] after:content-[''] before:h-4 before:bottom-0 after:bottom-0 after:h-4 tracking-tight font-[550] inline-block mx-2`,
+        `relative isolate rounded-3xl text-white px-3 py-px before:absolute after:absolute before:content-[''] after:content-[''] before:h-4 before:bottom-0 after:bottom-0 after:h-4 tracking-tight font-[550] inline-block mx-2`,
         direction === `right` &&
-          `before:right-[-7px] before:w-5 before:rounded-bl-2xl  bg-gray-600 before:bg-gray-600 before:z-[-1] after:right-[-26px] after:w-[26px] after:bg-white after:rounded-bl-[10px] after:z-[-1]`,
+          `before:right-[-7px] before:w-5 before:rounded-bl-2xl bg-gray-600 before:bg-gray-600 before:z-[-1] after:right-[-26px] after:w-[26px] after:bg-white after:rounded-bl-[10px] after:z-[-1]`,
         direction === `left` &&
           `before:left-[-7px] before:w-5 before:rounded-br-2xl bg-blue-600 before:bg-blue-600 before:z-[-1] after:left-[-26px] after:w-[26px] after:bg-white after:rounded-br-[10px] after:z-[-1]`,
         className
