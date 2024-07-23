9ffcc0ba0e83e5f9918740daf2127481edf9c1b3
contigen <69436315+contigen@users.noreply.github.com>
2024-07-23 17:19:57 +0100

    Add sheet UI

---
+++ src/features/ui/sheet.tsx
@@ -0,0 +1,42 @@
+import { Drawer } from 'vaul'
+import { ReactNode, ReactPortal, useState } from 'react'
+import clsx from 'clsx'
+import { Button } from './button'
+
+export function Sheet({
+  content,
+  children,
+}: {
+  children: ReactNode
+  content: ReactNode
+  className?: string
+}) {
+  const [dragged, setDragged] = useState(false)
+  return (
+    <div>
+      <Drawer.Root
+        shouldScaleBackground
+        onDrag={() => setDragged(true)}
+        onRelease={() => setDragged(false)}
+      >
+        <Drawer.Trigger asChild>
+          <Button className='bg-black text-white'>{children}</Button>
+        </Drawer.Trigger>
+        <Drawer.Portal>
+          <Drawer.Overlay className='fixed inset-0 bg-black/40 backdrop-blur-sm' />
+          <Drawer.Content className='flex flex-col rounded-t-3xl mt-24 fixed bottom-0 left-0 right-0 max-h-[80%] min-h-[50%] outline-none'>
+            <div className='p-4 bg-white rounded-t-3xl flex-1'>
+              <div
+                className={clsx(
+                  `mx-auto w-28 md:w-32 h-1.5 flex-shrink-0 rounded-full bg-zinc-200 hover:bg-zinc-300/90 mb-8 transition`,
+                  dragged && `bg-zinc-400/65 shadow-md`
+                )}
+              />
+              <div className='text-center'>{content as ReactPortal}</div>
+            </div>
+          </Drawer.Content>
+        </Drawer.Portal>
+      </Drawer.Root>
+    </div>
+  )
+}
