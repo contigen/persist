23f5c61ce44cbb38ed5bdaefc4755a031dacf961
contigen <69436315+contigen@users.noreply.github.com>
2024-07-26 13:34:32 +0100

    Modified toast options

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,15 @@ import { Toaster } from 'sonner'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-    <Toaster position='top-center' />
+    <Toaster
+      position='top-center'
+      toastOptions={{
+        classNames: {
+          toast:
+            'bg-white rounded-xl px-6 py-2 flex items-center gap-2 shadow-md border justify-center',
+        },
+      }}
+      duration={1000}
+    />
   </React.StrictMode>
 )
