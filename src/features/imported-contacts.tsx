b91e0a9ebf9d5d95c73fdf41045e44e521b720a8
contigen <69436315+contigen@users.noreply.github.com>
2024-07-27 16:54:43 +0100

    Add conditional render of button

--- src/features/imported-contacts.tsx
+++ src/features/imported-contacts.tsx
@@ -32,26 +32,28 @@ export function ImportedContacts({ vCardText }: { vCardText: string }) {
           <div className='space-y-4'>
             <h1 className='font-[650]'>Imported Contacts</h1>
             {importedContacts.length > 0 && (
-              <ol className='list-inside list-decimal'>
-                {importedContacts.map(({ name, tel }) => (
-                  <li key={name}>
-                    <b>{name}</b>{' '}
-                    <a
-                      href={`tel:${tel}`}
-                      className='underline underline-offset-2'
-                    >
-                      {tel}
-                    </a>
-                  </li>
-                ))}
-              </ol>
+              <>
+                <ol className='list-inside list-decimal'>
+                  {importedContacts.map(({ name, tel }) => (
+                    <li key={name}>
+                      <b>{name}</b>{' '}
+                      <a
+                        href={`tel:${tel}`}
+                        className='underline underline-offset-2'
+                      >
+                        {tel}
+                      </a>
+                    </li>
+                  ))}
+                </ol>
+                <Button
+                  className='bg-red-500 text-white shadow-md px-14'
+                  onClick={() => setImportedContacts([])}
+                >
+                  Remove all
+                </Button>
+              </>
             )}
-            <Button
-              className='bg-red-500 text-white shadow-md px-14'
-              onClick={() => setImportedContacts([])}
-            >
-              Remove all
-            </Button>
           </div>
         }
       >
